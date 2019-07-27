03e5b0426b9badb251ab4760f4948bd4a429de93
Hrvoje Jagnjic <hrvoje.jagnjic@gmail.com>
2019-07-27 12:13:39 +0200

    Variable coercion

--- script.js
+++ script.js
@@ -1,1 +1,60 @@
-console.log('Hello World!!!');
+/*
+Variables and data types
+*/
+/*var firstName = "John";
+console.log(firstName);
+
+var lastName = "Smith";
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = "Teacher";
+console.log(job);
+
+// Variable naming rules
+var _$3years = 3;
+var johnMark = "John and Mark";
+var if = 23;
+*/
+
+/* Variable mutation and type coercion */
+
+var firstName = "John";
+var age = 28;
+console.log(firstName + " " + age);
+
+var job, isMarried;
+job = "teacher";
+isMarried = false;
+// Type coercion
+console.log(
+  firstName +
+    " is a " +
+    age +
+    " year old " +
+    job +
+    ". Is he married? " +
+    isMarried
+);
+
+// Variable mutation
+age = "twenty eight";
+job = "driver";
+alert(
+  firstName +
+    " is a " +
+    age +
+    " year old " +
+    job +
+    ". Is he married? " +
+    isMarried
+);
+
+prompt("What is his last Name?");
+var lastName = prompt("What is his last name?");
+console.log(firstName + " " + lastName);
