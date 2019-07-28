3b80a9be821faa9dfa3b0516f8d5a7b7e9e4f984
Hrvoje Jagnjic <hrvoje.jagnjic@gmail.com>
2019-07-28 08:55:24 +0200

    Multiple operators & Grouping

--- script.js
+++ script.js
@@ -84,3 +84,16 @@ console.log(typeof ageJohn);
 console.log(typeof "Mark is older than John");
 var x;
 console.log(typeof x);
+
+/* Operator precedence*/
+var now = 2018;
+var yearMark = 1989;
+var fullAge = 18;
+// Multiple operators
+var isFullAge = now - yearJohn >= fullAge;
+console.log(isFullAge);
+// Grouping
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+var average = (ageJohn + ageMark) / 2;
+console.log(average);
