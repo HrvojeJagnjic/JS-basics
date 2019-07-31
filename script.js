575145e273a297b68576e5c81e23beadf2aef628
Hrvoje Jagnjic <hrvoje.jagnjic@gmail.com>
2019-07-31 17:26:19 +0200

    Falsy and truthsy

--- script.js
+++ script.js
@@ -194,3 +194,64 @@ if (age < 13) {
 } else {
   console.log(firstName + " is a man.");
 }
+
+/* the ternary Operator and switch statements*/
+
+var firstName = "John";
+var age = 20; // If we set it to 22 it will say john  drinks beer
+
+age >= 18
+  ? console.log(firstName + " drinks beer")
+  : console.log(firstName + " drinks juice");
+
+var drink = age >= 18 ? "beer" : "juice";
+console.log(drink);
+// This is the same
+/*if (age >= 18) { 
+  var drink = "beer";
+} else {
+  var drink = "juice";
+}*/
+
+// Switch statement
+var job = "teacher"; //driver or designer
+switch (job) {
+  case "teacher":
+  case "instructor":
+    console.log(firstName + " teaches kids how to code.");
+    break;
+  case "driver":
+    console.log(firstName + " drives an uber in Lisbon.");
+    break;
+  case "designer":
+    console.log(firstName + " designs beautiful websites.");
+    break;
+  default:
+    console.log(firstName + " does something else.");
+}
+age = 56;
+switch (true) {
+  case age < 13:
+    console.log(firstName + " is a boy.");
+    break;
+  case age >= 13 && age < 20:
+    console.log(firstName + " is a teenager.");
+    break;
+  case age >= 20 && age < 30:
+    console.log(firstName + " is a youngman.");
+    break;
+  default:
+    console.log(firstName + " is a man.");
+}
+
+/* truthy and Falsy values and equality operators*/
+// falsy values: undefined, null, 0, '', NaN
+
+//Truthy values: Not falsy values
+
+var height;
+if (height) {
+  console.log("Variable is defined");
+} else {
+  console.log("Variable has NOT been defined");
+}
